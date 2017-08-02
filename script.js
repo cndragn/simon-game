cafc2d4707d450851217e09019841d95cc906bab
CNDavidson <cndragn@gmail.com>
2017-08-02 09:12:28 -0400

    Create start button function to get new move array

--- script.js
+++ script.js
@@ -1,52 +1,60 @@
 $(document).ready(function() {
 
-//create random series of button presses
-var blocks = ["red", "green", "blue", "yellow"];
-var play = "";
-var series = [];
+    //create random series of button presses
+    var blocks = ["red", "green", "blue", "yellow"];
+    var play = "";
+    var series = [];
 
-function random() {
-    var move = Math.floor((Math.random() * 4));
-    if (move === 0) {
-        return "red";
+    function random() {
+        var move = Math.floor((Math.random() * 4));
+        if (move === 0) {
+            return "red";
+        }
+        if (move === 1) {
+            return "blue";
+        }
+        if (move === 2) {
+            return "green";
+        }
+        if (move === 3) {
+            return "yellow";
+        }
     }
-    if (move === 1) {
-        return "blue";
+
+    function moves() {
+        for (i = 0; i < 20; i++) {
+            play = random();
+            series.push(play);
+        }
     }
-    if (move === 2) {
-        return "green";
+
+    //Power up or power down
+    document.getElementById("power").addEventListener("click", onOff);
+
+    var pow = false;
+
+    function onOff() {
+        if (pow === false) {
+            console.log("Im on now!");
+            document.getElementById("count").innerHTML = "00";
+            pow = true;
+            start();
+        } else if (pow) {
+            console.log("Off now...");
+            document.getElementById("count").innerHTML = "";
+            pow = false;
+        }
     }
-    if (move === 3) {
-        return "yellow";
+
+    function start() {
+
+
+        document.getElementById("start").onclick = function() {
+            if (pow === true) {
+                series = [];
+                moves();
+                console.log(series);
+            }
+        }
     }
-}
-
-function newGame() {
-    for (i = 0; i < 20; i++) {
-        play = random();
-        series.push(play);
-    }
-}
-
-newGame();
-console.log(series.length);
-
-//Power up to start game play
-
-var pow = false;
-
-document.getElementById("power").addEventListener("click", onOff);
-
-function onOff(){
-	if(pow === false){
-		console.log("Im on now!");
-		document.getElementById("count").innerHTML = "00";
-		pow = true;
-	} else if (pow) {
-		console.log("Off now...");
-		document.getElementById("count").innerHTML = "";
-		pow = false;
-	}
-}
-
 });
