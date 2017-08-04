68c2415bf05bf602b43ea0a257e7b82201e33ea9
CNDavidson <cndragn@gmail.com>
2017-08-04 18:12:58 -0400

    Add player moves to an array for compare

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
     //create random series of button presses
     var blocks = ["red", "green", "blue", "yellow"];
     var play = "";
-    var series = [];
+    var aiSeries = [];
+    var playerSeries = [];
+    var counter = 0;
 
+//Select a random play
     function random() {
         var move = Math.floor((Math.random() * 4));
         if (move === 0) {
@@ -20,11 +23,11 @@ $(document).ready(function() {
             return "yellow";
         }
     }
-
+//add random play to aiSeries
     function moves() {
         for (i = 0; i < 20; i++) {
             play = random();
-            series.push(play);
+            aiSeries.push(play);
         }
     }
 
@@ -46,28 +49,23 @@ $(document).ready(function() {
         }
     }
 
-    function flash() {
-
-    }
-
+    //Start / Restart Button function
     function start() {
-
-
         document.getElementById("start").onclick = function() {
             if (pow === true) {
-                series = [];
                 moves();
-                console.log(series);
-
-
-                for (i = 0; i < series.length; i++) {
-                    var next = document.getElementById(series[i]);
-                    if (next = series[i]) {
-                        //Flash color to show next game play                   
-                        $("#" + series[i]).fadeTo(100, 0.1).fadeTo(200, 1.0);
-                    }
-                }
+                console.log(aiSeries);
+                 $("#" + aiSeries[0]).fadeTo(100, 0.1).fadeTo(200, 1.0);
+                 document.getElementById("colors").addEventListener("click", player);             
             }
         }
     }
+
+    function player(k) {
+        var key = k.target.id;
+        playerSeries.push(key); 
+        console.log(key);    
+        console.log(playerSeries);
+    }
+
 });
