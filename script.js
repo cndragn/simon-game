cc5dd6e68b0077c2d6471aed4b993302db96db75
CNDavidson <cndragn@gmail.com>
2017-08-05 14:02:55 -0400

    Add button state to start, strict and power

--- script.js
+++ script.js
@@ -49,104 +49,117 @@ $(document).ready(function() {
         if (pow === false) {
             //console.log("Im on now!");
             document.getElementById("count").innerHTML = "00";
+            document.getElementById("power").className += " active";
             pow = true;
             start();
         } else if (pow) {
             //console.log("Off now...");
             document.getElementById("count").innerHTML = "- -";
+            document.getElementById("power").className -= " active";
+            document.getElementById("strict").className -= " active";
+            document.getElementById("start").className -= " active";
             pow = false;
         }
     }
 
     //Start / Restart Button function
     function start() {
-        document.getElementById("start").onclick = function() {
-            if (pow === true) {
-                playerSeries = [];
-                if (aiSeries == "") {
-                    moves();
+        if (pow === true) {
+            document.getElementById("start").onclick = function() {
+                document.getElementById("start").className += " active";
+                if (pow === true) {
+                    playerSeries = [];
+                    if (aiSeries == "") {
+                        moves();
+                    }
+                    if (strMode === true) {
+                        aiSeries = [];
+                        moves();
+                    }
+                    makeMove();
                 }
-                if (strMode === true) {
-                    aiSeries = [];
-                    moves();
-                }
-                makeMove();
             }
         }
     }
 
     document.getElementById("strict").onclick = function() {
-        if (strMode === true) {
-            strMode = false;;
-        } else if (strMode === false) {
-            strMode = true;
+        if (pow === true) {
+            if (strMode === true) {
+                document.getElementById("strict").className -= " active";
+                strMode = false;
+            } else if (strMode === false) {
+                document.getElementById("strict").className += " active";
+                strMode = true;
+            }
         }
     }
 
     var iCount = 0;
 
     function sound(key) {
-        if(key === "red"){
+        if (key === "red") {
             auRed.play();
             //console.log("Play Red Sound!");
         }
-        if(key === "green"){
+        if (key === "green") {
             auGrn.play();
             //console.log("Play Green Sound!");
         }
-        if(key === "blue"){
+        if (key === "blue") {
             auBlu.play();
             //console.log("Play Blue Sound!");
         }
-        if(key === "yellow"){
+        if (key === "yellow") {
             auYel.play();
             //console.log("Play Yellow Sound!");
         }
     }
 
     function makeMove() {
-        //console.log(iCount);
-        //console.log(counter);
-        if (iCount <= counter) {
-            setTimeout(function() {
-                //console.log(aiSeries);
-                $("#" + aiSeries[iCount]).fadeTo(300, 0.1).fadeTo(500, 1.0);
-                sound(aiSeries[iCount]);
-                iCount++;
-                makeMove();
-            }, 1000)
+        if (pow === true) {
+            //console.log(iCount);
+            //console.log(counter);
+            if (iCount <= counter) {
+                setTimeout(function() {
+                    //console.log(aiSeries);
+                    $("#" + aiSeries[iCount]).fadeTo(300, 0.1).fadeTo(500, 1.0);
+                    sound(aiSeries[iCount]);
+                    iCount++;
+                    makeMove();
+                }, 1000)
+            }
+            //console.log(aiSeries);
+
+            /*
+            for (i = 0; i <= counter; i++) {
+                $("#" + aiSeries[i]).fadeTo(300, 0.1).fadeTo(500, 1.0);
+                //console.log(aiSeries[i]);
+
+            }
+            */
+            document.getElementById("colors").addEventListener("click", player);
         }
-        //console.log(aiSeries);
-
-        /*
-        for (i = 0; i <= counter; i++) {
-            $("#" + aiSeries[i]).fadeTo(300, 0.1).fadeTo(500, 1.0);
-            //console.log(aiSeries[i]);
-
-        }
-        */
-        document.getElementById("colors").addEventListener("click", player);
-
     }
 
     function player(k) {
-        iCount = 0;
-        var key = k.target.id;
+        if (pow === true) {
+            iCount = 0;
+            var key = k.target.id;
 
-        //console.log(key);
-        $("#" + key).fadeTo(300, 0.1).fadeTo(500, 1.0);
-        sound(key);
+            //console.log(key);
+            $("#" + key).fadeTo(300, 0.1).fadeTo(500, 1.0);
+            sound(key);
 
-        var match = "";
-        playerSeries.push(key);
+            var match = "";
+            playerSeries.push(key);
 
-        //console.log("Ai: " + aiSeries);
-        //console.log("Player: " + playerSeries);
-        var pLength = playerSeries.length;
-        //console.log("Player length: " + (pLength -= 1));
-        //console.log("Counter: " + counter);
+            //console.log("Ai: " + aiSeries);
+            //console.log("Player: " + playerSeries);
+            var pLength = playerSeries.length;
+            //console.log("Player length: " + (pLength -= 1));
+            //console.log("Counter: " + counter);
 
-        for (i = 0; i <= counter; i++) {
+            for (i = 0; i <= counter; i++) {
                 if (playerSeries[i] === aiSeries[i]) {
                     match = true
                     //console.log(match);
@@ -158,37 +171,38 @@ $(document).ready(function() {
                 }
             }
 
-        if ((playerSeries.length - 1) === counter) {
+            if ((playerSeries.length - 1) === counter) {
 
-            if (match === true) {
-                counter += 1;
-                if(counter < 10) {
-                    document.getElementById("count").innerHTML = "0" + counter;
-                } else {
-                    //console.log(counter);
-                document.getElementById("count").innerHTML = counter;
+                if (match === true) {
+                    counter += 1;
+                    if (counter < 10) {
+                        document.getElementById("count").innerHTML = "0" + counter;
+                    } else {
+                        //console.log(counter);
+                        document.getElementById("count").innerHTML = counter;
+                    }
+
+                    if (counter === 20) {
+                        //console.log("You Win!!!");
+                        auWin.play();
+                    }
+                    playerSeries = [];
+                    setTimeout(function() { makeMove(); }, 2000)
                 }
-                
-                if (counter === 20) {
-                    //console.log("You Win!!!");
-                    auWin.play();                  
+
+                if (match === false) {
+                    //console.log("Nope!");
+                    auLost.play();
+                    playerSeries = [];
+
+                    if (strMode === true) {
+                        aiSeries = [];
+                        setTimeout(function() { moves(); }, 3000)
+                    }
+                    setTimeout(function() { makeMove(); }, 3000)
                 }
-                playerSeries = [];
-               setTimeout(function() { makeMove(); }, 2000) 
+
             }
-
-            if (match === false) {
-                //console.log("Nope!");
-                auLost.play();
-                playerSeries = [];
-
-                if (strMode === true) {
-                    aiSeries = [];
-                    setTimeout(function() { moves(); }, 3000) 
-                }
-                setTimeout(function() { makeMove(); }, 3000) 
-            }
-
         }
 
     }
